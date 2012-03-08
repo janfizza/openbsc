a22e598c0a93f88433c6a00bd2acdde5c2f496d5
Andreas Eversberg <jolly@eversberg.eu>
2012-03-08 18:16:18 +0100

    Addition to commit ea724c6af9e1a5b6df8d0e5965f357896834d3ce

--- openbsc/src/libtrau/rtp_proxy.c
+++ openbsc/src/libtrau/rtp_proxy.c
@@ -242,6 +242,19 @@ static void tv_difference(struct timeval *diff, const struct timeval *from,
 	diff->tv_sec = to->tv_sec - from->tv_sec;
 }
 
+static void tv_add(struct timeval *tv, int sec, int usec)
+{
+
+	if (usec < 0)
+		usec += 1000000;
+	tv->tv_sec += sec;
+	tv->tv_usec += usec;
+	if (tv->tv_usec >= 1000000) {
+		tv->tv_sec++;
+		tv->tv_usec -= 1000000;
+	}
+}
+
 /* encode and send a rtp frame */
 int rtp_send_frame(struct rtp_socket *rs, struct gsm_data_frame *frame)
 {
@@ -263,6 +276,7 @@ int rtp_send_frame(struct rtp_socket *rs, struct gsm_data_frame *frame)
 		rs->transmit.ssrc = rand();
 		rs->transmit.sequence = random();
 		rs->transmit.timestamp = random();
+		gettimeofday(&rs->transmit.last_tv, NULL);
 	}
 
 	switch (frame->msg_type) {
@@ -307,14 +321,18 @@ int rtp_send_frame(struct rtp_socket *rs, struct gsm_data_frame *frame)
 
 		gettimeofday(&tv, NULL);
 		tv_difference(&tv_diff, &rs->transmit.last_tv, &tv);
-		rs->transmit.last_tv = tv;
+		tv_add(&rs->transmit.last_tv, 0, 20000);
 
 		usec_diff = tv_diff.tv_sec * 1000000 + tv_diff.tv_usec;
 		frame_diff = ((usec_diff + 10000) / 20000); /* round */
 
-		if (abs(frame_diff - 1) > 0) {
+		if (abs(frame_diff - 1) > 1) {
 			long int frame_diff_excess = frame_diff - 1;
+			long int sample_diff_excess = frame_diff_excess * duration;
 
+			/* correct last_tv */
+			tv_add(&rs->transmit.last_tv, sample_diff_excess / 8000,
+				(sample_diff_excess % 8000) * 125);
 			/* drop frame, if time stamp is in the past */
 			if (frame_diff_excess < 0)
 				return 0;
@@ -323,7 +341,7 @@ int rtp_send_frame(struct rtp_socket *rs, struct gsm_data_frame *frame)
 				"(to %s)\n", frame_diff_excess,
 				(rs->rx_action == RTP_RECV_L4) ? "app" : "BTS");
 			rs->transmit.sequence += frame_diff_excess;
-			rs->transmit.timestamp += frame_diff_excess * duration;
+			rs->transmit.timestamp += sample_diff_excess;
 		}
 	}
 
